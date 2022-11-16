b3c43bacdaf18d5376719f46964c37652845a44e
vas <vas.grishko@yandex.ru>
2022-11-17 00:03:19 +0300

    Вернули обратно команду

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,12 +2,17 @@
 int number1 = Convert.ToInt32(Console.ReadLine()); 
 Console.WriteLine("Введите число: ");
 int number2 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число: ");
+int number3 = Convert.ToInt32(Console.ReadLine()); 
 
-if (number1 > number2)
-Console.WriteLine("Первое число больше чем второе число"); 
+if (number1 > number2 && number1 > number3)
+Console.WriteLine("Первое число больше остальных чисел"); 
 
-if (number2 > number1)
-Console.WriteLine("Второе число больше чем первое число"); 
+if (number2 > number1 && number2 > number3)
+Console.WriteLine("Второе число больше остальных чисел"); 
 
-if (number2 == number1)
-Console.WriteLine("Числа равны"); 
+if (number3 > number1 && number3 > number2)
+Console.WriteLine("Третье число больше остальных чисел"); 
+
+
+
