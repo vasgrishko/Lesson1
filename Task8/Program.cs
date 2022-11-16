eabccdcdfd3306422a42cde69e500cc31f80ceb8
vas <vas.grishko@yandex.ru>
2022-11-17 02:47:00 +0300

    1-й вариант кода

---
+++ Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+int number2 = 0;
+
+while (number2 <= number1) 
+{
+   Console.Write(number2 + ", ");
+   number2 ++; 
+if (number2 % 2 == 0)
+{
+   Console.Write(number2); 
+}
+
+}
+   
