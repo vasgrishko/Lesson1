6f6108ccab82817b5c83baaa630d51b5f3c28a1b
vas <vas.grishko@yandex.ru>
2022-11-17 02:52:13 +0300

    правильный вариант кода

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,15 +1,14 @@
 ﻿Console.WriteLine("Введите число: ");
 int number1 = Convert.ToInt32(Console.ReadLine()); 
-int number2 = 0;
+int number2 = 1;
 
 while (number2 <= number1) 
 {
+   if (number2 % 2 == 0)
+
    Console.Write(number2 + ", ");
    number2 ++; 
-if (number2 % 2 == 0)
-{
-   Console.Write(number2); 
 }
 
-}
+
    
