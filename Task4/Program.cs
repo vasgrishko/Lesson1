d44e6584783869ea69fb424f537d655f1603cbac
vas <vas.grishko@yandex.ru>
2022-11-16 22:16:42 +0300

    добавили файл git ignore

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine()); 
+
+if (number1 > number2)
+Console.WriteLine("Первое число больше чем второе число"); 
+
+if (number2 > number1)
+Console.WriteLine("Второе число больше чем первое число"); 
+
+if (number2 == number1)
+Console.WriteLine("Числа равны"); 
