53a34ef3341320b49dec3404243e828fc5997279
vas <vas.grishko@yandex.ru>
2022-11-17 00:46:14 +0300

    Напиcали команду на проверку четности числа

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+if (number % 2 == 0)
+   Console.WriteLine("Четное число"); 
+else
+   Console.WriteLine("Нечетное число"); 
